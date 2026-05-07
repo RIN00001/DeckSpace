ef0b4a26f44305e5a1f06e7338dbe729002038e8
RIN <hamerboy37@gmail.com>
2026-05-07 13:05:17 +0700

    Initial Commit

---
+++ DeckSpace/DeckSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeckSpaceApp.swift
+//  DeckSpace
+//
+//  Created by student on 07/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DeckSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
